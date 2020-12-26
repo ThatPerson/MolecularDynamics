e77afea76e6119eb0bd9775d86836930a6e8ff9c
Ben Tatman <ben@tatmans.co.uk>
2020-12-26 17:10:39 +0000

    Temperature kinda works maybe? Needs work..

--- script.r
+++ script.r
@@ -3,7 +3,7 @@ bond 1.55
 %output output.xyz
 model MM3
 energy
-minimize 0.0001 0.00001 20000 100 min.xyz
+minimize 0.0001 0.00001 40000 100 min.xyz
 energy
 %heat .01
-prod 0.0001 0.00001 100 0.1 50000 100 prod.xyz
+prod 0.0001 0.00001 1 0.1 50000 100 prod.xyz
